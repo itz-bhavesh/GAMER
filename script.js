ee320e37a1004b67dd78f71e53211688b4532cad
alpha <119260445+itz-bhavesh@users.noreply.github.com>
2022-11-29 02:23:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Gamer", "Developer", "Traveller", "Thoughtful", "Respectful"],
+        strings: ["Gamer", "Developer", "Traveller", "Editor", "Youtuber"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Gamer", "Developer", "Traveller", "Thoughtful", "Respectful"],
+        strings: ["Gamer", "Developer", "Traveller", "Editor", "Youtuber"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
